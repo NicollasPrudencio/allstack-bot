d1d535fbe24bf4cf100f95ccce0c7d6e35e9187c
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-05-07 17:36:02 -0300

    test

--- server.js
+++ server.js
@@ -1,35 +1,56 @@
+const { Client } = require('whatsapp-web.js');
 const express = require('express');
-const bodyParser = require('body-parser');
-
+const qrcode = require('qrcode-terminal');
 const app = express();
-const port = 4567;
-let numReq = 0;
+const port = 80;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.post('/payload', (req, res) => {
-    numReq++;
+// Configurar o cliente do WhatsApp
+const client = new Client();
 
-    const githubToken = process.env.GITHUB_TOKEN;
-
-    // Verifica se o token está presente
-    if (!githubToken) {
-        return res.status(401).send('Token de acesso não configurado');
-    }
-
-    // Verifica a autenticação
-    const receivedToken = req.headers['x-hub-signature'];
-    // Faça aqui a validação adequada do token recebido, comparando com o token esperado
-
-    if (receivedToken !== githubToken) {
-        return res.status(403).send('Token de acesso inválido');
-    }
-
-    const push = req.body;
-    console.log(`I got some JSON (${numReq}):`, push);
-    res.sendStatus(200);
+// Autenticar usando o código QR
+client.on('qr', (qrCode, scanStatus) => {
+    qrcode.generate(qrCode, { small: true }); // Renderizar o código QR no terminal
+    console.log('Escaneie o código QR com o seu aplicativo WhatsApp:');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+// Evento de autenticação bem-sucedida
+client.on('authenticated', (session) => {
+    console.log('Autenticado no WhatsApp.');
+    startListening(); // Iniciar o servidor após a autenticação
 });
+
+// Inicializar o cliente do WhatsApp
+client.initialize();
+
+// Iniciar o servidor
+function startListening() {
+    app.post('/payload', (req, res) => {
+        const { commits } = req.body;
+
+        if (commits && commits.length > 0) {
+            const commit = commits[0];
+            const commitMessage = commit.message;
+            const commitAuthor = commit.author.username;
+            const commitURL = commit.url;
+            const commitDate = new Date(commit.timestamp).toLocaleString(); // Obter a data formatada
+
+            // Enviar a mensagem para o grupo específico no WhatsApp
+            const message = `Novo commit no repo de back end:\n\nNome: ${commitMessage}\nUsuário: ${commitAuthor}\nLocal: ${commitURL}\nData: ${commitDate}`; // Adicionar a data na última linha da mensagem
+            const groupID = 'EIRMKudB0CP3WZGm1SQwnm'; // Substitua pelo ID do grupo específico
+
+            client.sendMessage(groupID, message).then(() => {
+                console.log('Mensagem enviada com sucesso!');
+            }).catch((error) => {
+                console.error('Erro ao enviar a mensagem:', error);
+            });
+        }
+
+        res.sendStatus(200);
+    });
+
+    app.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port}`);
+    });
+}
